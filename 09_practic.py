050d5c225615095b52c14c9d7dbce102d31efa9a
Nurzaada <Nurzaada45@example.com>
2025-05-19 11:08:11 +0600

    Добавлены ф

---
+++ 09_practic.py
@@ -0,0 +1,43 @@
+# 'asdasdasd'  # str, строка
+# 40  # int, число
+# 50.33  # float, число с дробью
+# True, False  # boolean
+# [1, 2, 3]  # list, списки
+# 'ASDZXCQWE'
+# 'asdzxcqwe'
+
+# # functions
+# print
+# str
+# input
+# int
+
+# name = 'Doka'
+# age = 22
+# print(name)
+# journal = {
+#     1: 'Nazira Akmatova',
+#     2: 'Akylai B',
+#     'boy': 'Jun',
+# }
+
+# # print(journal['girl'])
+# value = journal.get('girl')
+# print(type(value))
+# print(type(journal))
+name = 'dairbek'
+last_name = 'SHEISHENALIEV'
+
+print(name.title())
+print(name.upper())
+print(last_name.lower())
+print(len(name))
+
+school = 'Лицей №57'
+      #   012345678
+# print(school[0])
+# print(school[-1])
+# print(school[0:5])
+# print(school[:5])
+# print(school[0:])
+print(school[2])
